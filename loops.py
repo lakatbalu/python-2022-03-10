5374bc2780c50c52542ab99778bf77a8acbf15b7
lakatbalu <lakatbalu15@gmail.com>
2022-03-10 15:08:19 +0100

    Loop

---
+++ loops.py
@@ -0,0 +1,15 @@
+
+for i in range(5):
+    print("Hello Python")
+    print(i)
+
+for i in range(6, 10):
+    print("Hello Python")
+    print(i)
+
+for i in [2,7,11,30]:
+    print(i)
+
+for name in ["john", "jack","jane"]:
+    print(name)
+
