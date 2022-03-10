8d3856e89c0e2cef5f108c541481e83b57041ad9
Monika Keller <monika.keller.szne@gmail.com>
2022-03-10 13:25:16 +0100

    Eljutottunk a kifejezések témakörig

---
+++ hello.py
@@ -0,0 +1,58 @@
+#Első python script
+# print(5)
+# print(5.2)
+# print("Hello World")
+#
+#
+# print(type(5))
+# print(type(5.2))
+# print(type(False))
+# print("""string
+# string""")
+# print('''string
+# string''')
+
+Pet = "Kutya"
+print(Pet)
+Pet = "Macska"
+print(Pet)
+
+print(type(Pet))
+
+wheel_of_car = 4
+print(type(wheel_of_car))
+
+pi = 3.14
+print(type(pi))
+
+#dinamikusan típusosság:
+houses = 12
+print(type(houses))
+houses = "sok"
+print(type(houses))
+
+#type hint:
+number_of_cars: int = 16
+
+number_of_cars_in_parking =number_of_cars
+
+print(len("alma"))
+length_of_apple = len('alma')
+print(length_of_apple)
+
+print(5.2*6.3)
+print(25/12)
+print(7.2-3)
+
+print(5.2*6.3)
+print(25/12)
+print(7.2-6)
+
+name = "John Doe"
+age = 40
+print(name, age, "eves")
+print(name + " " +str(age) + " eves")
+print(f"A {name} emberke {age} éves")
+print(name, age, "eves", sep="-**-")
+print("alma"+"körte")
+
