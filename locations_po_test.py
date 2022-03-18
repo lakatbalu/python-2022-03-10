488465165cbb58481c580a46b07b3fb47f615ef8
lakatbalu <lakatbalu15@gmail.com>
2022-03-18 15:45:14 +0100

    struktura változtatás

--- locations_po_test.py
+++ locations_po_test.py
@@ -1,11 +1,17 @@
 
+from sqlite3 import DatabaseError
 from LocationsMainPage import LocationsMainPage
 import requests
+import mariadb
 
 
 def test_create(driver):
     
-    requests.delete("http://localhost:8080/api/locations")
+    #requests.delete("http://localhost:8080/api/locations")
+
+    conn = mariadb.connect(user="location",password="locations", host="localhost", database="locations")
+    cur = conn.cursor()
+    cur.execute("delete from locations")
 
     page = LocationsMainPage(driver)
     page.open()
